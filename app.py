5fa55a48ec684d57336428ba80a2be0d1feb4365
Satyam_jaysawal <sjrecm9258@gmail.com>
2024-10-16 21:47:01 +0530

    first

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def create_app():
 # This is the part Flask will look for
 app = create_app()
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    import os
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
+
